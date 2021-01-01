f62202d87611e40e00dbf3cc49d25e4cde3a1dbb
Cole <cole2247@gmail.com>
2021-01-01 03:46:31 -0500

    rearragend some items

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-## Out of Stock
-Will be used to notify the user via their email if url for an item that they have requested is now in stock.
+# Out of Stock
+A application to notify the user if the url for an item that they have requested is now in stock.
+As of right now the following stores are suported :  
+- www.amazon.ca
+- www.newegg.ca
+- www.bestbuy.ca
+- www.canadacomputers.com
+
+## Methods of Notification
+The methods of notification are as follows. All methods have terminal as well:
+- Email
+- Terminal
+- System Notifacations (Linux only)
--- Remind.py
+++ Remind.py
@@ -14,11 +14,13 @@ import Utils
 
 class Task(ABC):
 
-    def __init__(self):
+    def __init__(self,silent =  False):
         self.json = Utils.JsonManager().json
         self.thread_count = self.json["threads"]
         self.urls = self.json["watch_list"]
         self.update_time = self.json["update_time"]
+        self.threads = []
+
 
     def identify_url(self,url):
         
@@ -28,30 +30,12 @@ class Task(ABC):
         
         return product(url)
 
+
     @abstractmethod
-    def notify(self):
+    def fire(self,product):
         pass
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-
-
-class TerminalView(Task):
-    def __init__(self):
-        super().__init__()
-
-        self.threads = []
-
-    def draw_update(self,products):
+    def check_produts(self,products):
         
         while True:
             for i in products:    
@@ -64,16 +48,24 @@ class TerminalView(Task):
                 message += instance.get_item_name()[:160] + " - "+ bcolors.OKCYAN + Utils.UrlUtils(i).extract_domain() + bcolors.ENDC
 
 
+                if start:
+                    self.fire(instance)
+
 
                 if status:
                     message += bcolors.OKGREEN + " - In Stock" + bcolors.ENDC
                 else:
                     message += bcolors.WARNING +" - Not In Stock" + bcolors.ENDC 
 
-                print(message)
+                if not silent:
+                    print(message)
+
             sleep(self.update_time)
 
-    def notify(self):
+
+
+
+    def assign_tasks(self):
         divided_tasks = np.array_split(self.urls,self.thread_count)
         
         for i in range(self.thread_count):
@@ -85,5 +77,69 @@ class TerminalView(Task):
         for i in self.threads:
             i.join()
 
-t = TerminalView()
-t.notify()
+
+
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+class TerminalView(Task):
+    def __init__(self):
+        super().__init__()
+
+
+
+    def fire(self,instance):
+        
+
+class EmailNotify(Task):
+    def __init__(self):
+        super().__init__()
+    
+
+    def send_notifacation(self):
+        import smtplib, ssl
+
+        port = 465  # For SSL
+        send_email = ""
+        receive_email = send_email
+        password = input("Type your password and press enter: ")
+
+        # Create a secure SSL context
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+            server.login(send_email, password)
+            server.sendmail(send_email, receive_email, "test123")
+            # TODO: Send email here
+
+
+    def notify(self):
+        pass
+
+
+t = EmailNotify()
+t.send_notifacation()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
